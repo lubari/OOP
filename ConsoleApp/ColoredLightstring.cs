a2cd090735fbd2494038fa2e8bc9251b190205a0
Ana <analubari@gmail.com>
2023-04-24 17:50:48 -0300

    task 6

---
+++ ConsoleApp/ColoredLightstring.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    internal class ColoredLightstring : Lightstring
+    {
+        private ColoredBulb[] coloredBulbs;
+        private int serialNumber;
+
+        public ColoredLightstring(int serialNumber, int quantity) { 
+            
+            coloredBulbs = new ColoredBulb[quantity];
+        }
+
+        private void SetBublsColors()
+        {
+            ColoredBulb.Color color;
+
+            if (this.serialNumber % 4 == 0)
+            {
+                color = ColoredBulb.Color.Blue;
+            }
+            else if (this.serialNumber% 2 == 0)
+            {
+                color = ColoredBulb.Color.Green;
+            }
+            else if (this.serialNumber % 2 == 0)
+            {
+                color = ColoredBulb.Color.Yellow;
+            }
+            else
+            {
+                color = ColoredBulb.Color.Red;
+            }
+
+            foreach(ColoredBulb b in coloredBulbs)
+            {
+                b.SetColor(color);
+            }
+        }
+    }
+}
